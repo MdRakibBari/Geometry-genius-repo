f12b49e26af1400baf285955185c12682752c09b
MdRakibBari <mdrakibbari01@gmail.com>
2024-02-18 18:09:55 +0600

    finally done in one hour I can't believe it

--- javaScript/triangle.js
+++ javaScript/triangle.js
@@ -1,6 +1,6 @@
 // Triangle Calculation
 
-function calculateTriangle(){
+function calculateTriangle() {
     const inputText1 = document.getElementById('inputBox1');
     const triangleTextBase = inputText1.value;
     const TriangleToNumber = parseFloat(triangleTextBase)
@@ -10,20 +10,23 @@ function calculateTriangle(){
     const heightToNumber = parseFloat(triangleTextheight)
 
     const area = 0.5 * TriangleToNumber * heightToNumber;
+
+    const TriangleAreaValue = document.getElementById('triangle-area-value');
+    TriangleAreaValue.innerText = area
     // console.log("Area of the Triangle is :", area)
 
-    const calculatorBox = document.getElementById('Show-the-result-here')
-    const p = document.createElement('p');
-    p.innerText = area;
-    p.style.fontSize = "50px"
-    calculatorBox.appendChild(p)
+    // const calculatorBox = document.getElementById('Show-the-result-here')
+    // const p = document.createElement('p');
+    // p.innerText = area;
+    // p.style.fontSize = "50px"
+    // calculatorBox.appendChild(p)
 
 }
 
 
 // Rectangle Calculation
 
-function calculateRectangle(){
+function calculateRectangle() {
     const inputText3 = document.getElementById('inputBox3');
     const rectangleTextBase = inputText3.value;
     const rectangleBase = parseFloat(rectangleTextBase)
@@ -33,5 +36,78 @@ function calculateRectangle(){
     const rectangleHeight = parseFloat(rectangleTextHeight)
 
     const area = rectangleBase * rectangleHeight;
-    console.log("Area of the Rectangle is :", area)
+
+    const rectangleAreaValue = document.getElementById('rectangleAreaValue');
+    rectangleAreaValue.innerText = area
+}
+
+
+// Parallelogram calculator
+
+function calculatorParallelogram() {
+    const ParallelogramInput = document.getElementById('parallelogramBase');
+    const parallelogramText = ParallelogramInput.value;
+    const Base = parseFloat(parallelogramText)
+
+
+    const ParallelogramSecondInput = document.getElementById('parallelogramHeight');
+    const ParallelogramHeight = ParallelogramSecondInput.value;
+    const height = parseFloat(ParallelogramHeight);
+
+    const AreaValue = Base * height;
+    const ParallelogramAreaValue = document.getElementById('parallelogramAreaValue');
+    ParallelogramAreaValue.innerText = AreaValue
+
+}
+
+
+// Rhombus Calculator
+
+function calculatorRhombus() {
+    const rhombusd1Input = document.getElementById('rhombusd1');
+    const rhombusd1Text = rhombusd1Input.value;
+    const valued1 = parseFloat(rhombusd1Text);
+
+    const rhombusd2Input = document.getElementById('rhombusd2');
+    const rhombusd2Text = rhombusd2Input.value;
+    const valued2 = parseFloat(rhombusd2Text);
+
+    const area = 0.5 * valued1 * valued2
+
+    const rhombusAreaValue = document.getElementById('rhombusAreaValue');
+    rhombusAreaValue.innerText = area
+}
+
+
+// Pentagon Calculator
+
+function calculatorPentagon() {
+    const PentagonValueP = document.getElementById('pentagonValueP');
+    const PentagonValuePText = PentagonValueP.value;
+    const valueP = parseFloat(PentagonValuePText);
+
+    const PentagonValueB = document.getElementById('pentagonValueB');
+    const PentagonValueBText = PentagonValueB.value;
+    const valueB = parseFloat(PentagonValueBText);
+
+    const area = 0.5 * valueP * valueB;
+    const PentagonAreaValue = document.getElementById('pentagonAreaValue');
+    PentagonAreaValue.innerText = area
+}
+
+
+// Ellipse Calculator 
+
+function calculatorEllipse() {
+    const ellipseValueA = document.getElementById('ellipsevalueA');
+    const ellipseValueAText = ellipseValueA.value;
+    const valueA = parseFloat(ellipseValueAText);
+
+    const ellipseValueB = document.getElementById('ellipsevalueB');
+    const ellipseValueBText = ellipseValueB.value;
+    const valueB = parseFloat(ellipseValueBText);
+
+    const area = 3.14 * valueA * valueB
+    const EllipseAreaValue = document.getElementById('ellipseAreaValue');
+    EllipseAreaValue.innerText = area
 }
